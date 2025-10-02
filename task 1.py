164f6d8a7a781ba53751bae6049183130ce541b0
NikolaySamsonov <65284383+NikolaySamsonov@users.noreply.github.com>
2025-10-02 14:00:31 +0300

    Add files via upload
    adding new file

---
+++ task 1.py
@@ -0,0 +1,8 @@
+word : str = (input())
+if not word:
+    print("Строка пустая")
+    exit()
+if len(word) % 2 != 0:
+    print(word[(len(word) // 2)])
+else:
+    print((word[len(word) // 2-1])+(word[(len(word) // 2)]))
